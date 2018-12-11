a2b86862dd98730f96c3416cf357cb12d4a94995
stewartadmin <stewartadmin@WinD3782.lasp.colorado.edu>
2018-12-11 10:51:54 -0700

    mergint

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ import json
 TAI_EPOCH = dt.datetime(1999, 12, 31, 23, 59, 23)  # Epoch time for incoming time stamps
 MAX_TIME = dt.datetime(2020, 1, 1, 12, 0, 0)  # max allowable time for naive filtering
 MIN_TIME = dt.datetime(2018, 1, 1, 12, 0, 0)  # minimum allowable time for naive filtering
-NETCDF_EPOCH = dt.datetime(2018, 1, 1)  # Epoch for netCDF file.
+NETCDF_EPOCH = dt.datetime(2000, 1, 1)  # Epoch for netCDF file.
 
 CONFIG_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "cube_ds_2.cfg")  # defines config file for configparser
 # Parsing the config file.
